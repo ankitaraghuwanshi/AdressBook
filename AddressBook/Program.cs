8a994188d3ec00007205c6ef72356e81748039f2
ankitaraghuwanshi <bittanraghu@gmail.com>
2022-03-21 14:31:26 +0530

    [Ankita] Add: program for adding contact

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -5,17 +5,74 @@ namespace AddressBook
 {
     class ContactsDetails
     {
+
         public string First_Name = "";
         public string Last_Name = "";
         public string Address = "";
         public string City = "";
         public string State = "";
-        public string Zip_Code = "";
-        public string Phone_Number = "";
+        public int Zip_Code =0;
+        public int Phone_Number = 1;
         public string Email = "";
 
-        public ContactsDetails()
+        public ContactsDetails(string First_Name, string Last_Name, string Address, string City, string State, int Zip_Code, int Phone_Number, string Email)
         {
+            this.First_Name = First_Name;
+            this.Last_Name = Last_Name;
+            this.Address = Address;
+            this.City = City;
+            this.State = State;
+            this.Zip_Code = Zip_Code;
+            this.Phone_Number = Phone_Number;
+            this.Email = Email;
+        }
+        public string tostring()
+        {
+            return "Details of " + First_Name + "  " + Last_Name + " are : " + " Address: " + Address + "City:" + City + "\n" 
+                                 +"                                  "+        " state - " + State + " Zip_Code -" + Zip_Code + "\n" 
+                                 +"                                  "+        " phone number -" + Phone_Number + "\n" 
+                                 +"                                 "+        "Email :" + Email;
+
+        }
+
+
+
+    }
+
+
+    class program
+    {
+        private ArrayList ContactDetailsList;
+        private Dictionary<string, ContactsDetails> ContactDetailsMap;
+        public program()
+        {
+            ContactDetailsList = new ArrayList();
+            ContactDetailsMap = new Dictionary<string, ContactsDetails>();
+        }
+
+
+
+        
+        public void AddDetails(string First_Name, string Last_Name, string Address, string City, string State, int zip_code,int phone_number, string Email)
+        {
+            ContactsDetails contactsDetails = new ContactsDetails(First_Name, Last_Name, Address, City, State, zip_code, phone_number, Email);
+            ContactDetailsList.Add(contactsDetails);
+            ContactDetailsMap.Add(First_Name, contactsDetails);
+        }
+        public void ComputeDetails()
+        {
+            foreach (ContactsDetails contact in ContactDetailsList)
+            {
+                Console.WriteLine(contact.tostring());
+
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Addressbook");
+            program details = new program();
+
             Console.WriteLine("Enter First_Name");
             string First_Name = Console.ReadLine();
 
@@ -39,37 +96,23 @@ namespace AddressBook
 
             Console.WriteLine("Enter Email");
             string Email = Console.ReadLine();
+
+            details.AddDetails(First_Name, Last_Name, Address, City, State, Zip_Code, Phone_Number, Email);
+
+            details.ComputeDetails();
+
+
+
+
+
+
+
+
+
         }
-        
-        class AddressBook
-        {
-            public static void Main(string[] agrs)
-            {
-                Console.WriteLine("Welcome to Address Book System");
-                ContactsDetails Book = new ContactsDetails();
-            }
-        }
+
     }
-
-}         
-                
-             
-
-        
-
-
-
-
-
-
-
-
-    
-
-
-
-
-
+}
 
 
 
